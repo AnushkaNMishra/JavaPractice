8db4951492980aa711f08a32d5dfd0d790f4623a
Anushka <anushkam306@gmail.com>
2022-04-09 20:46:14 +0530

    Initial Commit

---
+++ test.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Test {
+    public static void name(String[] args) {
+        Scanner ob=new Scanner (System.in);
+        System.out.println("enter roll no");
+        int roll=ob.nextInt();
+        System.out.println("roll no is "+roll);
+    }
+    
+}
