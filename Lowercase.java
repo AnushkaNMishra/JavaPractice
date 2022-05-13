4aa65d18d20d90adb2734bd634e44e29a7aee0bf
Anushka N Mishra <84915436+AnushkaNMishra@users.noreply.github.com>
2022-05-13 14:17:36 +0530

    Create Lowercase.java

---
+++ Lowercase.java
@@ -0,0 +1,8 @@
+public class Lowercase {
+    public static void main(String[] args){
+        String str = "HeLLoWoRLd";
+        String result = str.toLowerCase();
+        System.out.println("Original String  : " + str);
+        System.out.println("Lowercase String : " + result);
+    }
+}
